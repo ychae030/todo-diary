9e72b17051cf94128f38e2c617f0bd50308c9d51
ychae1997 <77264092+ychae1997@users.noreply.github.com>
2024-09-24 01:47:42 +0900

    Chore: Build Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // color 추가
+        brand: "#3f72af",
+        brandDark: "#dbe2ef",
+        danger: "#e76c6c",
+        dangerDark: "#dbe2ef",
+      },
+    },
+    fontFamily: {
+      // FontFamily 추가
+      LOTTERIACHAB: ["LOTTERIACHAB"],
+      LeeSeoyun: ["LeeSeoyun"],
+    },
   },
   plugins: [],
-}
-
+  darkMode: "selector", // darkMode 추가
+};
