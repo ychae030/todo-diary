e7527984e96efe6b88f3ae8351106cc332ac73f9
ychae1997 <77264092+ychae1997@users.noreply.github.com>
2024-09-24 01:17:51 +0900

    Feat: Create Router

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,56 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
+import NotFound from "./pages/NotFound";
+import TodoList from "./pages/todo/TodoList";
+import Calendar from "./pages/diary/Calendar";
+import Layout from "./components/common/Layouts/Layout";
+import MainHeader from "./components/common/header/MainHeader";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        index: true,
+        element: (
+          <Layout
+            header={{ header: <MainHeader /> }}
+            content={{ children: <TodoList /> }}
+          />
+        ),
+      },
+      {
+        path: "todo",
+        element: (
+          <Layout
+            header={{ header: <MainHeader /> }}
+            content={{ children: <TodoList /> }}
+          />
+        ),
+      },
+      {
+        path: "diary",
+        element: (
+          <Layout
+            header={{ header: <MainHeader /> }}
+            content={{ children: <Calendar /> }}
+          />
+        ),
+      },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
---
+++ src/pages/todo/TodoList.tsx
@@ -0,0 +1,3 @@
+export default function TodoList() {
+  return <div>오늘 할 일</div>;
+}
