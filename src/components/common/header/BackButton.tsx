d7b32da61cf949875516eee7f3503994c720ddca
ychae1997 <77264092+ychae1997@users.noreply.github.com>
2024-10-02 00:32:31 +0900

    Feat: 뒤로가기 버튼

--- src/components/common/header/BackButton.tsx
+++ src/components/common/header/BackButton.tsx
@@ -1,5 +1,12 @@
 import React from "react";
+import { IoIosArrowBack } from "react-icons/io";
+import { useNavigate } from "react-router-dom";
 
 export default function BackButton() {
-  return <div></div>;
+  const navigate = useNavigate();
+  return (
+    <button onClick={() => navigate(-1)}>
+      <IoIosArrowBack />
+    </button>
+  );
 }
