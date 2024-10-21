6f364547f61890f21a73a66404e412533cd7c431
ychae030 <ychae1997@gmail.com>
2024-10-21 12:16:52 +0900

    Fix: todo 체크 반영 바로 안되는 현상 수정

--- src/components/todo/TodoItem.tsx
+++ src/components/todo/TodoItem.tsx
@@ -3,6 +3,7 @@ import { FaCheck, FaPencil } from "react-icons/fa6";
 import useModal from "../../hooks/useModal";
 import TodoInputModal from "./TodoInputModal";
 import { updateTodo } from "../../api/todoApi";
+import { useMutation } from "react-query";
 
 export type Status = "active" | "completed";
 export type TodoItemType = {
@@ -20,12 +21,26 @@ export default function TodoItem({ item, mode }: TodoItemProps) {
   const [isChecked, setIsChecked] = useState<boolean>(
     item.status === "completed"
   );
+
   const { isOpen, openModal, closeModal } = useModal();
 
+  // Mutation for updating the todo item
+  const mutation = useMutation(
+    (newStatus: Status) => updateTodo(item.id, { status: newStatus }),
+    {
+      onError: () => {
+        // 만약 에러가 발생하면 UI 상태를 롤백하거나 사용자에게 오류 알림
+        setIsChecked(item.status === "completed"); // 상태 롤백
+      },
+    }
+  );
+
   const handleCheck = async () => {
-    const newStatus = item.status === "completed" ? "active" : "completed"; // 상태를 반대로 전환
-    setIsChecked(newStatus === "completed"); // 새로운 상태에 따라 isChecked 업데이트
-    updateTodo(item.id, { status: newStatus });
+    const newStatus = isChecked ? "active" : "completed"; // 반대 상태로 전환
+    setIsChecked(!isChecked); // UI 먼저 업데이트 (낙관적 업데이트)
+
+    // 서버에 상태 업데이트 요청
+    mutation.mutate(newStatus);
   };
 
   return (
@@ -43,18 +58,20 @@ export default function TodoItem({ item, mode }: TodoItemProps) {
           className="inline-flex gap-2 cursor-pointer items-center"
         >
           <span
-            className={`flex items-center justify-center border border-brand w-5 h-5 rounded-full box-border ${
-              isChecked ? "bg-brand" : ""
+            className={`flex items-center justify-center border border-brand w-5 h-5 rounded-full box-border dark:border-dangerDark ${
+              isChecked ? "bg-brand dark:bg-brandDark" : ""
             }`}
           >
-            {isChecked && <FaCheck className="absolute text-xs text-white" />}
+            {isChecked && (
+              <FaCheck className="absolute text-xs text-white dark:text-slate-800" />
+            )}
           </span>
           <strong className="font-normal">{item.text}</strong>
         </label>
       </div>
-      {mode === "future" && (
+      {!mode && (
         <button onClick={openModal}>
-          <FaPencil className="text-xs" />
+          <FaPencil className="text-xs text-brand dark:text-brandDark" />
         </button>
       )}
 
